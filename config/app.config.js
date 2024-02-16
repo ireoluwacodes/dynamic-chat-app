42369363aebd0da281a35a8727363f41a6ee0342
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-02-16 23:21:09 +0100

    initialized socket io

--- config/app.config.js
+++ config/app.config.js
@@ -1,8 +1,14 @@
 import express from "express";
+import http from "http"
+import socketIo from "socket.io"
 import cors from "cors"
 import { appRouter } from "../routes/app.route.js";
 
 export const app = express();
+
+export const server = http.createServer(app)
+
+export const io = socketIo(server)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
