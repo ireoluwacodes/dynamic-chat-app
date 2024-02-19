6c31cd9b2af2c2641c0521ad2a1b4873b85dac09
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-02-19 22:22:59 +0100

    working...

--- config/app.config.js
+++ config/app.config.js
@@ -1,35 +1,35 @@
 import express from "express";
-import http from "http"
-import socketIo from "socket.io"
-import cors from "cors"
+import http from "http";
+import socketIo from "socket.io";
+import cors from "cors";
 import { appRouter } from "../routes/app.route.js";
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 app.use(appRouter);
 
 // socket shits
-export const server = http.createServer(app)
+export const server = http.createServer(app);
 
-export const io = socketIo(server)
+export const io = socketIo(server);
 
-const usp = io.of("/user-namespace")
+const usp = io.of("/user-namespace");
 
-const updateUserOnlineStatus = (socket)=>{
-    console.log("User Connected")
+const updateUserOnlineStatus = (socket) => {
+  console.log("User Connected");
 
-    const id = socket.handshake.auth.id
+  const id = socket.handshake.auth.id;
 
+  socket.broadcast.emit("currentlyOnline", { id });
 
-    socket.broadcast.emit("currentlyOnline", {id: user._id})
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("currentlyOffline", { id });
+    console.log("User Disconnected");
+  });
+};
 
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected")
-    })
-}
-
-usp.on("connection", updateUserOnlineStatus)
+usp.on("connection", updateUserOnlineStatus);
